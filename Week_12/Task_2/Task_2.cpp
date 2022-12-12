ecdc72f0feac5729eceeafbfa2a37dce62970f92
Kostiantyn <krokotkoleshnia@gmail.com>
2022-12-12 16:04:39 +0200

    Week_12 commit

---
+++ Week_12/Task_2/Task_2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int time_past;
+	struct
+	{
+		int hours;
+		int minutes;
+	} FirstDate, SecondDate, Difference;
+	cout << "Введіть значення годин та хвилин і час (в хвилинах).\n";
+	cin >> FirstDate.hours >> FirstDate.minutes >> SecondDate.hours >> SecondDate.minutes ;
+
+	if ((FirstDate.hours > 24 && FirstDate.minutes > 60) || (SecondDate.hours > 24 && FirstDate.minutes > 60)) {
+		cout << "Incorrect data";
+	}
+	else {
+		if (SecondDate.hours < FirstDate.hours) 
+		{
+			Difference.hours = 24 - FirstDate.hours + SecondDate.hours;
+		}
+		else {
+			Difference.hours = SecondDate.hours - FirstDate.hours;
+		}
+		
+		if (SecondDate.minutes < FirstDate.minutes) {
+			Difference.hours--;
+			Difference.minutes = 60 + SecondDate.minutes - FirstDate.minutes;
+		}
+		else
+		{
+			Difference.minutes = SecondDate.minutes - FirstDate.minutes;
+		}
+
+		cout << "Difference: " << Difference.hours << ":" << Difference.minutes;
+	}
+}
---
+++ Week_12/Week_12/Task_1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int time_past;
+	struct
+	{
+		int hours;
+		int minutes;
+	} Date;
+    cout << "Введіть значення годин та хвилин і час (в хвилинах).\n";
+	cin >> Date.hours >> Date.minutes >> time_past;
+
+	if (Date.hours > 24 && Date.minutes > 60) {
+		cout << "Incorrect data";
+	}
+	else {
+		Date.minutes = Date.minutes + time_past;
+
+		while (Date.minutes > 60) {
+			Date.minutes -= 60;
+			Date.hours += 1;
+		}
+		while (Date.hours > 24) {
+			Date.hours -= 24;
+		}
+		cout << "Expected time: " << Date.hours << ":" << Date.minutes;
+	}
+}
